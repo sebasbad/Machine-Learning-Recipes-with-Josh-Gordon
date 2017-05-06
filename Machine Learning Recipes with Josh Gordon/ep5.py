778db169669570f3f56246eec0bb742ade4598fc
Sebastian Badea <sebastian.badea@infojobs.net>
2017-05-06 23:34:49 +0200

    Updated ep5 to add scrappy kin classifier implementation

--- Machine Learning Recipes with Josh Gordon/ep5.py
+++ Machine Learning Recipes with Josh Gordon/ep5.py
@@ -1,18 +1,31 @@
 # Writing Our First Classifier - Machine Learning Recipes #5
-import random
+from scipy.spatial import distance
+
+def euc(a, b):
+    return distance.euclidean(a, b)
 
 class ScrappyKNN():
     def fit(self, X_train, y_train):
-        self.X_Train = X_train
+        self.X_train = X_train
         self.y_train = y_train
 
     def predict(self, X_test):
         predictions = []
         for row in X_test:
-            label = random.choice(self.y_train)
+            label = self.closest(row)
             predictions.append(label)
 
         return predictions
+
+    def closest(self, row):
+        best_dist = euc(row, self.X_train[0])
+        best_index = 0
+        for i in range(1, len(self.X_train)):
+            dist = euc(row, self.X_train[i])
+            if dist < best_dist:
+                best_dist = dist
+                best_index = i
+        return self.y_train[best_index]
 
 from sklearn import datasets
 iris = datasets.load_iris()
